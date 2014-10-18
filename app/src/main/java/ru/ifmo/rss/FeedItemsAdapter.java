0ae093f246dea7796b03c2e2e1dd487b99ccf11c
Zakhar Voit <zakharvoit@gmail.com>
2014-10-18 20:30:35 +0400

    More beautiful appearance

--- app/src/main/java/ru/ifmo/rss/FeedItemsAdapter.java
+++ app/src/main/java/ru/ifmo/rss/FeedItemsAdapter.java
@@ -1,6 +1,7 @@
 package ru.ifmo.rss;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,10 +15,12 @@ import java.util.List;
 public class FeedItemsAdapter extends BaseAdapter {
     List<FeedItem> items;
     Context context;
+    LayoutInflater inflater;
 
     public FeedItemsAdapter(List<FeedItem> items, Context context) {
         this.items = items;
         this.context = context;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -38,9 +41,15 @@ public class FeedItemsAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
-            convertView = new TextView(context);
+            convertView = inflater.inflate(R.layout.item_row, null);
         }
-        ((TextView)convertView).setText(items.get(position).getDescription());
+
+        TextView title = (TextView) convertView.findViewById(R.id.title);
+        TextView description = (TextView) convertView.findViewById(R.id.description);
+
+        FeedItem item = items.get(position);
+        title.setText(item.getTitle());
+        description.setText(item.getDescription());
 
         return convertView;
     }
